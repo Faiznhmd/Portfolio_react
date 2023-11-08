1963c744251284be526969b72c6dd2c94d91f1a0
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-11-08 23:44:32 +0530

    pages added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Services from './components/Services';
+function App() {
+  return (
+    <>
+      <Home />
+      <Services />
+      <About />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
